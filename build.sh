5bc0e6de41ac370546769b0e18f3c56a9c7267fa
Yuki Tsujii (laptop) <yuki.tsujii.37@gmail.com>
2024-05-28 00:09:55 +0900

    tinyfix

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gulp build --modules=appnexusBidAdapter,dgkeywordRtdProvider
-cp build/dist/prebid.js ../../adtest/main/public/hb-relay/
+gulp build --modules=appnexusBidAdapter,dgkeywordRtdProvider,mediaConsortiumBidAdapter
+cp build/dist/prebid.js ../adtest/main/public/hb-relay/
